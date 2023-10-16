bea8bf20fd4f2c04a7fc1b1cb7e3cfedd0d5858d
Victor Hugo Grabowski Beltramini <vhbeltramini@gmail.com>
2023-10-16 20:24:55 -0300

    initial commit

---
+++ src/test/java/com/vhbeltramini/grp/GrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhbeltramini.grp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
